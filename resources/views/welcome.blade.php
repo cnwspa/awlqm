024f88cf3593c67a5ebc7c0730657b314feb8538
avrcmz <jcomeaux@ug.uchile.cl>
2022-07-17 11:39:01 -0400

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
         <!-- Logo cloud -->
         <div class="bg-gray-100">
           <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-            <p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide"> NUESTROS CLIENTES EXIJEN CALIDAD</p>
+            <p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide"> NUESTROS CLIENTES EXIGEN CALIDAD</p>
             <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
               <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                 <img class="h-12" src="{{asset('img/home/essity.svg')}}" alt="Tuple">
