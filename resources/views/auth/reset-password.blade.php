37af3823bfee7bae752db12fa0128e88b3f8fee4
avrcmz <jcomeaux@ug.uchile.cl>
2022-07-24 19:16:06 -0400

    up

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -28,7 +28,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Resetear contraseña') }}
+                    {{ __('Reestablecer contraseña') }}
                 </x-jet-button>
             </div>
         </form>
