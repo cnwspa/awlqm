c32ba4e5b966f8ed00855290b0ffb1b0fd133e53
avrcmz <jcomeaux@ug.uchile.cl>
2022-07-23 20:59:06 -0400

    Pasword

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -15,13 +15,13 @@
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="password" value="{{ __('New Password') }}" />
+            <x-jet-label for="password" value="{{ __('Nueva Contraseña') }}" />
             <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
             <x-jet-input-error for="password" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+            <x-jet-label for="password_confirmation" value="{{ __('Confirmar Contraseña') }}" />
             <x-jet-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
             <x-jet-input-error for="password_confirmation" class="mt-2" />
         </div>
