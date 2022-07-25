c92846c50ecdd65ccca55b7eb1abcd98a0f69f06
avrcmz <jcomeaux@ug.uchile.cl>
2022-07-25 08:56:16 -0400

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,8 +21,8 @@
                   <span class="block text-white">Nuestro objetivo es</span>
                   <span class="block text-indigo-200">Tu tranquilidad</span>
                 </h1>
-                <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-1xl">Con más de 20 años en ruta desde Arica a Punta Arenas, 
-                    ofrecemos el servicio más seguro y ágil de cara a tus clientes</p>
+                <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-1xl">Más de 20 años en ruta desde Arica a Punta Arenas, 
+                    brindando la máxima calidad en transporte de carga</p>
                 <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                   <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
                     <a href="{{ route('login') }}" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"> Ingresar </a>
