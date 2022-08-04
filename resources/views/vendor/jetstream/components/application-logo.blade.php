2993ec668b1137c76ac525f24bd79cf6693564b5
4v3cmxz <jcomeaux@ug.uchile.cl>
2022-08-04 00:08:44 -0400

    up

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <figure>
-  <img class="max-h-16 w-16" src="{{asset('img/logo.jpg')}}" alt="">
+  <img class="max-h-32 w-32" src="{{asset('img/logo.jpg')}}" alt="">
 </figure>
--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,3 +1,3 @@
 <figure>
-  <img class="max-h-16 w-16" src="{{asset('img/logo.jpg')}}" alt="">
+  <img class="max-h-20 w-20" src="{{asset('img/logo.jpg')}}" alt="">
 </figure>
