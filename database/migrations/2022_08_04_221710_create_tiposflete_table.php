2408ed1a10bbdd80b1d130ae9baeca31808947e2
4v3cmxz <jcomeaux@ug.uchile.cl>
2022-08-05 11:49:50 -0400

    up

--- database/migrations/2022_08_04_221710_create_tiposflete_table.php
+++ database/migrations/2022_08_04_221710_create_tiposflete_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user_fletes', function (Blueprint $table) {
+        Schema::create('tiposflete', function (Blueprint $table) {
             $table->id();
-
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('flete_id');
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
-            $table->foreign('flete_id')->references('id')->on('fletes')->onDelete('set null');
-
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_fletes');
+        Schema::dropIfExists('tiposflete');
     }
 };
--- database/migrations/2022_08_05_153309_create_fletes_table.php
+++ database/migrations/2022_08_05_153309_create_fletes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('fletes', function (Blueprint $table) {
             $table->id();
-            $table->date('inicio');
+            $table->integer('valor');
             $table->string('guia');
             $table->integer('bultos');
             $table->integer('volumen');
@@ -23,22 +23,23 @@ return new class extends Migration
             $table->integer('cantidad');
             $table->integer('viatico');
             $table->integer('comision');
-            $table->integer('valor');
+            $table->date('inicio');
             $table->date('termino');
 
-            $table->unsignedBigInteger('origen_id');
-            $table->unsignedBigInteger('destino_id');
-            $table->unsignedBigInteger('tipoflete_id');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('tarifa_id');
-            $table->unsignedBigInteger('descripcion_id');
+            $table->unsignedBigInteger('origen_id')->nullable();
+            $table->unsignedBigInteger('destino_id')->nullable();
+            $table->unsignedBigInteger('tipoflete_id')->nullable();
+            $table->unsignedBigInteger('tarifa_id')->nullable();
+            $table->unsignedBigInteger('descripcion_id')->nullable();
 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('origen_id')->references('id')->on('origenes')->onDelete('set null');
             $table->foreign('destino_id')->references('id')->on('destinos')->onDelete('set null');
             $table->foreign('tipoflete_id')->references('id')->on('tiposflete')->onDelete('set null');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
             $table->foreign('tarifa_id')->references('id')->on('tarifas')->onDelete('set null');
             $table->foreign('descripcion_id')->references('id')->on('descripciones')->onDelete('set null');
+
             $table->timestamps();
         });
     }
