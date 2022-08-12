186d04e37226ed2177c2c55d440a63104289c885
4v3cmxz <jcomeaux@ug.uchile.cl>
2022-08-12 10:37:31 -0400

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="shortcut icon" href="{{asset('img/logo.jpg')}}">
+        <link rel="shortcut icon" href="{{asset('img/LOGITO_FINAL.png')}}">
         <title>{{ config('app.name', 'Transportes LQM SpA') }}</title>
 
         <!-- Fonts -->
